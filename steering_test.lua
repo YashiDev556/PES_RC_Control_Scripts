2eb514d9c4daec96511ea25c1ee73df88f74d939
YashiDev556 <80170466+YashiDev556@users.noreply.github.com>
2025-04-10 21:36:08 -0400

    Update steering_test.lua
    updated radio channels

--- steering_test.lua
+++ steering_test.lua
@@ -16,8 +16,8 @@ function update()
     local current_pos = ahrs:get_location()
     -- local home = ahrs:get_home()
 
-    local controlC = rc:get_pwm(5) -- change this to be the same channels as C and D
-    local controlD = rc:get_pwm(6)
+    local controlC = rc:get_pwm(14) -- change this to be the same channels as C and D
+    local controlD = rc:get_pwm(15)
 
     if controlC > 1500 then
         SRV_Channels:set_output_pwm(94, output_speed) -- set the servo assigned function 96 (scripting1) to a proportional value
@@ -27,7 +27,6 @@ function update()
         SRV_Channels:set_output_pwm(94, -output_speed) -- scripting1
     end
     
-
     return update, 50 -- call again in some amount of milliseconds
 end
 
